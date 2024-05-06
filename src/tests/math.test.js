8975e9bdee897d6fb8e27398b5333213840463df
Adrien Albuquerque <adrialb95@gmail.com>
2024-05-06 23:29:49 +0200

    Tests

---
+++ src/tests/math.test.js
@@ -0,0 +1,13 @@
+// math.test.js
+describe("Tests for math functions", () => {
+  const addition = (a, b) => a + b;
+  const soustraction = (a, b) => a - b;
+
+  test("addition 1 + 2 to equal 3", () => {
+    expect(addition(1, 2)).toBe(3);
+  });
+
+  test("soustraction 5 - 2 to equal 3", () => {
+    expect(soustraction(5, 2)).toBe(3);
+  });
+});
